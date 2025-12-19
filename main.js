b75c07c657e18c504f6d0e86dc167543c3a000a2
Luc Jeandupeux <e228323@edu.sbl.ch>
2025-12-19 13:58:28 +0000

    WIP: Timer eingebaut, Back-Link von Unterseite überarbeitet

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 function changeColor(color) {
   document.documentElement.style.setProperty("--section-color", color);
 }
+
+// Erstellt eine Variable mit Namen count und dem Wert 0
+let count = 0;
+
+// Erstellt eine Funktion mit dem Namen increaseCount
+function increaseCount() {
+  // Erstellt eine neue Variable mit Namen titleElement und speichert das fieldset-Element darin
+  let titleElement = document.querySelector("fieldset");
+
+  // Erhöht die Variable count um 1
+  count = count + 1;
+
+  // setzt den Textinhalt von titleElement auf den Wert von count.
+  titleElement.textContent = count;
+}
+setInterval(increaseCount, 1000);
